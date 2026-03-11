c2bfb4d34bbaedf4218578a72d9d44b82f7ffd85
Jonas Bork <jonas10bork@gmail.com>
2026-03-11 16:31:06 +0100

    Update internal url

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -7,7 +7,7 @@ variable "harbor_url" {
 variable "harbor_internal_url" {
   description = "kubernetes url"
   type        = string
-  default     = "harbor.harbor.svc.cluster.local"
+  default     = "artifact-registry-harbor-portal.harbor.svc.cluster.local"
 }
 
 variable "harbor_admin_user" {
